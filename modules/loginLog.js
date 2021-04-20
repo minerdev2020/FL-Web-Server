a9e960ea00048af554c5d4f346212988894579ba
minerdev <minerdev2020@gmail.com>
2021-04-20 15:37:46 +0900

    style: 로그인 로그의 날짜 형식 변경

--- modules/loginLog.js
+++ modules/loginLog.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
+const moment = require('moment');
 
 module.exports = {
   writeLog: (userInfo) => {
     console.log(userInfo);
+    const now = moment().format('YYYY-MM-DD HH:mm:ss ZZ');
     fs.appendFile(
       path.join(__dirname, `../logs/login_${userInfo.id}.log`),
-      `${new Date()} ${userInfo.user_id} ${userInfo.ip}\n`,
+      `[${now}] ${userInfo.user_id} ${userInfo.ip}\n`,
       (err) => {
         if (err) {
           throw err;
