1844b10ea6bf445fa0c17b812fde31e2b06735a9
minerdev <minerdev2020@gmail.com>
2021-04-21 18:41:07 +0900

    feat : 센서 데이터 그래프 출력 기능 추가

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function init() {
     });
 
   app.use(morgan('dev'));
-  // app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser(process.env.COOKIE_SECRET));
