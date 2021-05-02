e4f0aa7e4bdb5150480482baba245d287130b8e0
minerdev <minerdev2020@gmail.com>
2021-05-02 06:41:04 +0000

    refactor: 로그인 로그 파일을 찾지 못 할때의 예외처리 추가

--- routes/log.js
+++ routes/log.js
@@ -8,20 +8,28 @@ router.get('/login/:id', async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { person_id: req.params.id } });
     if (user) {
-      const logs = await loginLog.readLog(user);
-      return res.status(200).json({
-        code: 200,
-        message: 'Succeeded!',
-        data: logs.toString(),
-      });
+      try {
+        const logs = await loginLog.readLog(user);
+        return res.status(200).json({
+          code: 200,
+          message: 'Succeeded!',
+          data: logs.toString(),
+        });
+      } catch (err) {
+        console.error(err);
+        return res.status(404).json({
+          code: 404,
+          message: "failed! such user's login log file doesn't exist!",
+        });
+      }
     }
     return res.status(404).json({
       code: 404,
       message: "failed! such user doesn't exist!",
     });
-  } catch (error) {
-    console.error(error);
-    return next(error);
+  } catch (err) {
+    console.error(err);
+    return next(err);
   }
 });
 
